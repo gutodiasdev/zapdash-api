2e5f8ea539a2f96ec87de361c87e4ec79bc69d3d
Augusto Dias <gutodiasux@gmail.com>
2022-12-01 19:21:59 -0300

    add dotenv

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
+import { PORT } from '../domain/config'
 
 const app = express()
 app.use(express.json())
 
-app.listen(5050, () => console.log('Running on port: 5050'))
+app.listen(PORT, () => console.log('Running on port: ' + PORT))
