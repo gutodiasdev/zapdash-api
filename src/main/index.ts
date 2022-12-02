8630708ce0bb39b0692c99da4a3b3f7b99cdc2e7
Augusto Dias <gutodiasux@gmail.com>
2022-12-02 11:41:26 -0300

    refactor: add errorMiddleware after routes handlers

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,15 @@
 import '../main/config/module-alias'
+import 'express-async-errors'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import { PORT } from './config'
+import { userRouter } from '@/domain/modules/user/routes/user.routes'
+import { errorMiddleware } from '@/main/middlewares'
 
 const app = express()
 app.use(express.json())
+app.use('/users', userRouter)
+app.use(errorMiddleware)
 
 app.listen(PORT, () => console.log('Running on port: ' + PORT))
