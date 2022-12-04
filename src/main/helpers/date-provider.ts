160a5a88435bf28c9a816c87cc5b4eaa6a6ee3e6
Augusto Dias <gutodiasux@gmail.com>
2022-12-04 20:37:51 -0300

    feat: add isExpired method to DateProvider

--- src/main/helpers/date-provider.ts
+++ src/main/helpers/date-provider.ts
@@ -2,10 +2,15 @@ import dayjs from 'dayjs'
 
 export interface DateProviderInterface {
   addDays(days: number): Date
+  isExpired(date: Date): boolean
 }
 
 export class DateProvider implements DateProviderInterface {
   addDays(days: number): Date {
     return dayjs().add(days, 'days').toDate()
+  }
+
+  isExpired(date: Date): boolean {
+    return dayjs().isAfter(date)
   }
 }
